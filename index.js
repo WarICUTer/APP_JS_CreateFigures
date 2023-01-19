672af04a99b708e357924629835cbbc8707f903f
Enrique Cuevas <juan.vrg.lg.712@gmail.com>
2023-01-19 13:42:42 -0600

    Update code and improbe the interface

--- index.js
+++ index.js
@@ -1,51 +1,111 @@
 const contenedor = document.querySelector(".contenedor");
-const cuadrado = document.getElementById("button-cuadrado")
-const rectangulo = document.getElementById("button-recta")
-const circulo = document.getElementById("button-circulo")
-const triangulo = document.getElementById("button-trian")
+const square = document.getElementById("button-cuadrado")
+const rectangle = document.getElementById("button-recta")
+const circle = document.getElementById("button-circulo")
+const triangle = document.getElementById("button-trian")
 let i = 0;
 
-function createFigure(className) {
-    const card = document.createElement('div');
+const opctionsFigure = (className, name, id) => {
+    const card = document.createElement('DIV');
+    const figure = document.createElement('DIV');
+    const title = document.createElement('H2');
+
+    figure.className = className;
+    title.textContent = name;
+    card.className = 'card-options';
+    card.addEventListener('click', e => {
+        const figure = document.getElementById(id)
+        figure.className = className;
+        document.querySelector('.modal').removeChild(document.querySelector('.options-modal'));
+    })
+
+    card.appendChild(figure);
+    card.appendChild(title);
+
+    return card;
+}
+
+const createFigure = className => {
+    const card = document.createElement('DIV');
+    const options = document.createElement('DIV');
+    const figure = document.createElement('DIV');
+    const deleteBtn = document.createElement('BUTTON'); 
+    const modifyBtn = document.createElement('BUTTON');
+
     card.className = 'card';
     card.id = `card${i}`
-    contenedor.appendChild(card)
-    const figure = document.createElement('div');
+
+    options.className = 'options';
+    
     figure.className = className;
-    card.appendChild(figure)
-    const deleteBtn = document.createElement('button'); 
+    figure.id = i;
+    
     deleteBtn.className = 'btn-delete';
-    deleteBtn.setAttribute('onClick','Eliminar(this)')
+    deleteBtn.textContent = 'Delete'; 
     deleteBtn.id = i;
-    deleteBtn.innerText = 'Eliminar'; 
+    deleteBtn.addEventListener('click', e => {
+        let id = parseInt(e.target.id);
+        const card = document.getElementById(`card${id}`);
+        contenedor.removeChild(card);
+    })
+    
+    modifyBtn.className = 'btn-delete btn-blue';
+    modifyBtn.textContent = 'Edit';
+    modifyBtn.id = i;
+    modifyBtn.addEventListener('click', e => {
+        let id = parseInt(e.target.id);
+        const editModal = updateFigure(id);
+        document.querySelector('.modal').appendChild(editModal)
+    })
+    
+    options.appendChild(figure);
+    card.appendChild(options);
     card.appendChild(deleteBtn);
-    const modifyBtn = document.createElement('button');
-    modifyBtn.className = 'btn-delete btn-blue'
-    modifyBtn.id = `btn${i}`
-    modifyBtn.innerText = 'Editar'
-    card.appendChild(modifyBtn)
-    i = i + 1;
+    card.appendChild(modifyBtn);
+    contenedor.appendChild(card);
+    
+    i += 1;
 }
 
-function Eliminar(botonPulsado){
-    let id = botonPulsado.id;
-    const card = document.getElementById(`card${id}`);
-    contenedor.removeChild(card)
+const updateFigure = id => {
+    const modal = document.createElement('DIV');
+    const options = document.createElement('DIV');
+    const title = document.createElement('H2');
+    const cancelBtn = document.createElement('BUTTON');
+    const square = opctionsFigure('shape-square', 'square', id)
+    const recta = opctionsFigure('shape-recta', 'Rectangle', id)
+    const circle = opctionsFigure('shape-circle', 'Circle', id)
+    const trian = opctionsFigure('shape-trian', 'Triangle', id)
+
+    modal.className = 'options-modal';
+
+    options.className = 'options'
+
+    title.className = 'title-options';
+    title.textContent = 'Chose the new shape';
+    
+    cancelBtn.className = 'btn-delete';
+    cancelBtn.textContent = 'Cancel';
+    cancelBtn.addEventListener('click', () => {
+        document.querySelector('.modal').removeChild(modal);
+    })
+
+    modal.appendChild(title);
+    options.appendChild(square);
+    options.appendChild(recta);
+    options.appendChild(circle);
+    options.appendChild(trian);
+    modal.appendChild(options)
+    modal.appendChild(cancelBtn);
+
+    return modal;
 }
 
-cuadrado.onclick = function(){
-    createFigure('shape-square')
-}
+square.addEventListener('click', () => createFigure('shape-square'));
 
-rectangulo.onclick = function(){
-    createFigure('shape-recta')
-}
+rectangle.addEventListener('click', () => createFigure('shape-recta'));
 
-circulo.onclick = function(){
-    createFigure('shape-circle')
-}
+circle.addEventListener('click', () => createFigure('shape-circle'));
 
-triangulo.onclick = function(){
-    createFigure('shape-trian')
-}
+triangle.addEventListener('click', () => createFigure('shape-trian'));
 
